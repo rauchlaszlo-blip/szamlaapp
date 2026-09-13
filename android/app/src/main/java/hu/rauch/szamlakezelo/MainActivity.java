716569e919e0095e51699c42aa077a9667bb38b6
rauchlaszlo-blip <rauchlaszlo@gmail.com>
2026-09-13 18:53:26 +0200

    Fix API 36 status bar overlap

--- android/app/src/main/java/hu/rauch/szamlakezelo/MainActivity.java
+++ android/app/src/main/java/hu/rauch/szamlakezelo/MainActivity.java
@@ -1,14 +1,29 @@
 package hu.rauch.szamlakezelo;
 
 import android.os.Bundle;
-import androidx.core.view.WindowCompat;
+import android.view.View;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         registerPlugin(DriveBackupPlugin.class);
-        WindowCompat.setDecorFitsSystemWindows(getWindow(), true);
         super.onCreate(savedInstanceState);
+
+        View webView = getBridge().getWebView();
+        ViewCompat.setOnApplyWindowInsetsListener(webView, (view, windowInsets) -> {
+            Insets statusBars = windowInsets.getInsets(WindowInsetsCompat.Type.statusBars());
+            view.setPadding(
+                view.getPaddingLeft(),
+                statusBars.top,
+                view.getPaddingRight(),
+                view.getPaddingBottom()
+            );
+            return windowInsets;
+        });
+        ViewCompat.requestApplyInsets(webView);
     }
 }
