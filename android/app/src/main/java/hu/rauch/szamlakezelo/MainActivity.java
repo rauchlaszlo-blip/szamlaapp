5f85623c4e1b72e339a579a14250e6fecec1fa8b
rauchlaszlo-blip <rauchlaszlo@gmail.com>
2026-09-13 17:21:21 +0200

    Use native non-edge-to-edge layout for Android header

--- android/app/src/main/java/hu/rauch/szamlakezelo/MainActivity.java
+++ android/app/src/main/java/hu/rauch/szamlakezelo/MainActivity.java
@@ -1,24 +1,14 @@
 package hu.rauch.szamlakezelo;
 
 import android.os.Bundle;
-import android.view.View;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.core.view.WindowCompat;
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         registerPlugin(DriveBackupPlugin.class);
+        WindowCompat.setDecorFitsSystemWindows(getWindow(), true);
         super.onCreate(savedInstanceState);
-
-        View webView = getBridge().getWebView();
-        ViewCompat.setOnApplyWindowInsetsListener(webView, (view, windowInsets) -> {
-            Insets statusBars = windowInsets.getInsets(WindowInsetsCompat.Type.statusBars());
-            view.setPadding(0, statusBars.top, 0, 0);
-            return windowInsets;
-        });
-        ViewCompat.requestApplyInsets(webView);
     }
 }
