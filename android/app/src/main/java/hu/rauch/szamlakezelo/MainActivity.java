e6acb80197b75f43cdc94dced792f60be42335c3
rauchlaszlo-blip <rauchlaszlo@gmail.com>
2026-09-13 16:57:12 +0200

    Fix Android top status bar inset

--- android/app/src/main/java/hu/rauch/szamlakezelo/MainActivity.java
+++ android/app/src/main/java/hu/rauch/szamlakezelo/MainActivity.java
@@ -1,6 +1,10 @@
 package hu.rauch.szamlakezelo;
 
 import android.os.Bundle;
+import android.view.View;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
@@ -8,5 +12,13 @@ public class MainActivity extends BridgeActivity {
     public void onCreate(Bundle savedInstanceState) {
         registerPlugin(DriveBackupPlugin.class);
         super.onCreate(savedInstanceState);
+
+        View webView = getBridge().getWebView();
+        ViewCompat.setOnApplyWindowInsetsListener(webView, (view, windowInsets) -> {
+            Insets statusBars = windowInsets.getInsets(WindowInsetsCompat.Type.statusBars());
+            view.setPadding(0, statusBars.top, 0, 0);
+            return windowInsets;
+        });
+        ViewCompat.requestApplyInsets(webView);
     }
 }
