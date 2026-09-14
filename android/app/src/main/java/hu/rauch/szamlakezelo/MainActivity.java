054325922ebcab336dbe972a308934b77301cbd4
rauchlaszlo-blip <rauchlaszlo@gmail.com>
2026-09-14 12:31:18 +0200

    Register invoice attachment plugin

--- android/app/src/main/java/hu/rauch/szamlakezelo/MainActivity.java
+++ android/app/src/main/java/hu/rauch/szamlakezelo/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends BridgeActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         registerPlugin(DriveBackupPlugin.class);
+        registerPlugin(InvoiceAttachmentPlugin.class);
         super.onCreate(savedInstanceState);
 
         getOnBackPressedDispatcher().addCallback(this, new OnBackPressedCallback(true) {
